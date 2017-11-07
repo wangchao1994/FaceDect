cc565f0726a7d4150630200e791a290862c07944
wangchao <wchao0829@163.com>
2017-11-07 15:39:26 +0800

    第一次提交

--- app/src/main/java/com/go/facedect/MainActivity.java
+++ app/src/main/java/com/go/facedect/MainActivity.java
@@ -1,13 +1,49 @@
 package com.go.facedect;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.face_detect);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.face_detect:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},1);
+                }else{
+                    startActivity(new Intent(MainActivity.this,DectorActivity.class));
+                }
+                break;
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startActivity(new Intent(MainActivity.this,DectorActivity.class));
+                }else{
+                    Toast.makeText(MainActivity.this,"权限拒绝",Toast.LENGTH_SHORT).show();
+                }
+               // break;
+        }
     }
 }
